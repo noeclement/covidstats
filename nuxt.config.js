d02bda2c858d4d63e50a961579c48fc2a6580243
Noé <noeclement@gmail.com>
2020-07-30 17:54:08 +0200

    Update bulma to use latest css spacing utilities

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ module.exports = {
   /*
   ** Global CSS
   */
-  css: [],
+  css: [{ src: 'bulma/sass/helpers/spacing.sass', lang: 'sass' }],
 
   /*
   ** Plugins to load before mounting the App
